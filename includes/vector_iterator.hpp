664893dd2a62b5f7da47d7c355d4c7bcc4097eed
Alexandre DE GARRIGUES <ade-garr@e2r9p18.42.fr>
2021-10-22 15:27:05 +0200

    vector iterator canonical form

--- includes/vector_iterator.hpp
+++ includes/vector_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: ade-garr <ade-garr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 16:08:48 by ade-garr          #+#    #+#             */
-/*   Updated: 2021/10/21 18:11:12 by ade-garr         ###   ########.fr       */
+/*   Updated: 2021/10/22 15:22:49 by ade-garr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,30 @@ namespace ft {
 	template< typename T >
 	class vector_iterator {
 
+		public:
+
 		// ----- MEMBER TYPES -----
 		typedef std::random_access_iterator_tag	iterator_category;
 		typedef T								value_type;
 		typedef ptrdiff_t						difference_type;
-		typedef T*								pointer;
-		typedef const T&						reference;
+		typedef value_type*								pointer;
+		typedef const value_type&						reference;
 
 		// ----- ITERATOR TRAITS -----
+		vector_iterator() : v_it(NULL) {};
+		vector_iterator(vector_iterator const &cpy) {
+			*this = cpy;
+		}
+		~vector_iterator() {}
+		vector_iterator &operator=(vector_iterator const &rhs) {
+			v_it = rhs.v_it;	// a voir si necessaire de rajouter le if *this != rhs
+			return (*this);
+		}
+
+		protected:
+
+		pointer	v_it;
+
 	};
 }
 
