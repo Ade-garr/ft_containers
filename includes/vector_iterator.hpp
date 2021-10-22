6b1c888d27518c1fe08df45a1613a9da48e62f55
Alexandre DE GARRIGUES <ade-garr@e2r9p18.42.fr>
2021-10-22 17:51:41 +0200

    vector iterator operators

--- includes/vector_iterator.hpp
+++ includes/vector_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: ade-garr <ade-garr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 16:08:48 by ade-garr          #+#    #+#             */
-/*   Updated: 2021/10/22 15:22:49 by ade-garr         ###   ########.fr       */
+/*   Updated: 2021/10/22 17:51:26 by ade-garr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,56 @@ namespace ft {
 		typedef std::random_access_iterator_tag	iterator_category;
 		typedef T								value_type;
 		typedef ptrdiff_t						difference_type;
-		typedef value_type*								pointer;
-		typedef const value_type&						reference;
+		typedef value_type*						pointer;
+		typedef const value_type&				reference;
 
 		// ----- ITERATOR TRAITS -----
 		vector_iterator() : v_it(NULL) {};
 		vector_iterator(vector_iterator const &cpy) {
 			*this = cpy;
 		}
+		// a voir si constructeur avec const iterator necessaire
 		~vector_iterator() {}
 		vector_iterator &operator=(vector_iterator const &rhs) {
 			v_it = rhs.v_it;	// a voir si necessaire de rajouter le if *this != rhs
 			return (*this);
+		}
+		bool operator==(vector_iterator const &rhs) {
+			if (v_it == rhs.v_it)
+				return (true);
+			return (false);
+		}
+		bool operator!=(vector_iterator const &rhs) {
+			if (v_it != rhs.v_it)
+				return (true);
+			return (false);
+		}
+		reference operator*() {
+			return (*v_it);
+		}
+		reference operator->() {
+			return (*v_it);
+		}
+		vector_iterator &operator++() {
+			v_it++;
+			return (*this);
+		}
+		vector_iterator &operator++(int) {
+			vector_iterator tmp = *this;
+			v_it++;
+			return (tmp);
+		}
+		vector_iterator &operator--() {
+			v_it--;
+			return (*this);
+		}
+		vector_iterator &operator--(int) {
+			vector_iterator tmp = *this;
+			v_it--;
+			return (tmp);
+		}
+		vector_iterator &operator+(vector iterator const &rhs) {
+			return (v_it + );
 		}
 
 		protected:
