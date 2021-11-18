7e76f1ebf62f8d88132bb61189c0a145833e6bfe
ade-garr <ade-garr@student.42.fr>
2021-11-18 14:00:47 +0100

    map iterator

--- includes/map_iterators.hpp
+++ includes/map_iterators.hpp
@@ -6,7 +6,7 @@
 /*   By: ade-garr <ade-garr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 10:47:11 by ade-garr          #+#    #+#             */
-/*   Updated: 2021/11/18 12:45:04 by ade-garr         ###   ########.fr       */
+/*   Updated: 2021/11/18 14:00:43 by ade-garr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,36 @@ namespace ft {
 		}
 		friend bool operator!=(map_iterator const &lhs, map_iterator const &rhs) {
 			return !(lhs._node == rhs._node);
+		}
+		reference operator*() const {
+			return (ptr->val);
+		}
+		pointer operator->() const {
+			return (&(ptr->val));
+		}
+		map_iterator& operator++() { // a voir
+			if (ptr == NULL)
+			{
+				m_node = m_endnode;
+				return (*this);
+			}
+			else if (m_node->right != NULL)
+			{
+				m_node = m_node->right;
+				while (m_node->left != NULL)
+					m_node = m_node->left;
+			}
+			else
+			{
+				Node* origin = m_node;
+				m_node = m_node->parent;
+				while (m_node && m_node->right == origin)
+				{
+					origin = m_node;
+					m_node = m_node->parent;
+				}
+			}
+			return (*this);
 		}
 
 		// ----- PRIVATE -----
