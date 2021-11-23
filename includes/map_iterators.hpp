6601234171139b93bdedfc3fe5555ba3308201ee
Ade-garr <ade-garr@student.42.fr>
2021-11-23 02:02:58 +0100

    map iterators

--- includes/map_iterators.hpp
+++ includes/map_iterators.hpp
@@ -6,7 +6,7 @@
 /*   By: ade-garr <ade-garr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 10:47:11 by ade-garr          #+#    #+#             */
-/*   Updated: 2021/11/18 14:00:43 by ade-garr         ###   ########.fr       */
+/*   Updated: 2021/11/23 01:59:17 by ade-garr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ namespace ft {
         typedef value_type&						reference;
 
         // ----- ITERATOR PROPERTIES -----
-		map_iterator(NodePtr node = NULL) : ptr(node) {}
+		map_iterator(node_ptr node = NULL) : ptr(node) {}
 		map_iterator(map_iterator const &cpy) {
             *this = cpy;
         }
@@ -53,38 +53,158 @@ namespace ft {
 		pointer operator->() const {
 			return (&(ptr->val));
 		}
-		map_iterator& operator++() { // a voir
-			if (ptr == NULL)
-			{
-				m_node = m_endnode;
+		map_iterator& operator++() {
+			if (ptr == NULL) {
 				return (*this);
 			}
-			else if (m_node->right != NULL)
-			{
-				m_node = m_node->right;
-				while (m_node->left != NULL)
-					m_node = m_node->left;
+			else if (ptr->right != NULL) {
+				ptr = ptr->right;
+				while (ptr->left != NULL)
+					ptr = ptr->left;
 			}
-			else
-			{
-				Node* origin = m_node;
-				m_node = m_node->parent;
-				while (m_node && m_node->right == origin)
-				{
-					origin = m_node;
-					m_node = m_node->parent;
+			else {
+				node_ptr origin = ptr;
+				ptr = ptr->parent;
+				while (ptr && ptr->right == origin) {
+					origin = ptr;
+					ptr = ptr->parent;
 				}
 			}
 			return (*this);
+		}
+		map_iterator operator++(int) {
+			map_iterator tmp(*this);
+			++(*this);
+			return (tmp);
+		}
+		map_iterator& operator--() {
+			if (ptr == NULL) {
+				return (*this);
+			}
+			else if (ptr->left != NULL) {
+				ptr = ptr->left;
+				while (ptr->right != NULL)
+					ptr = ptr->right;
+			}
+			else {
+				node_ptr origin = ptr;
+				ptr = ptr->parent;
+				while (ptr && ptr->left == origin) {
+					origin = ptr;
+					ptr = ptr->parent;
+				}
+			}
+			return (*this);
+		}
+		map_iterator operator--(int) {
+			map_iterator tmp(*this);
+			--(*this);
+			return (tmp);
 		}
 
 		// ----- PRIVATE -----
 		private:
 
 		// ----- MEMBER TYPES -----
-		typedef ft::map< ft::T::first_type, ft::T::second_type >::Node* NodePtr;
+		typedef ft::map< ft::T::first_type, ft::T::second_type >::Node* node_ptr;
 
-		NodePtr ptr;
+		node_ptr ptr;
+	};
+
+	template< typename T >
+    class map_const_iterator {
+
+		// ----- PUBLIC -----
+        public:
+
+        // ----- MEMBER TYPES -----
+        typedef std::bidirectional_iterator_tag	iterator_category;
+        typedef T								value_type;
+        typedef ptrdiff_t						difference_type;
+        typedef const value_type*				pointer;
+        typedef const value_type&				reference;
+
+        // ----- ITERATOR PROPERTIES -----
+		map_const_iterator(node_ptr node = NULL) : ptr(node) {}
+		map_const_iterator(map_const_iterator const &cpy) {
+            *this = cpy;
+        }
+		map_const_iterator(map_iterator< value_type > const &it) : ptr(it.operator->()) {}
+		~map_const_iterator() {}
+		map_const_iterator &operator=(map_const_iterator const &rhs) {
+            if (*this != rhs)
+				this->ptr = rhs.ptr;
+			return (*this);
+		}
+		friend bool operator==(map_const_iterator const &lhs, map_const_iterator const &rhs) {
+			return (lhs.ptr == rhs.ptr);
+		}
+		friend bool operator!=(map_const_iterator const &lhs, map_const_iterator const &rhs) {
+			return !(lhs._node == rhs._node);
+		}
+		reference operator*() const {
+			return (ptr->val);
+		}
+		pointer operator->() const {
+			return (&(ptr->val));
+		}
+		map_const_iterator& operator++() {
+			if (ptr == NULL) {
+				return (*this);
+			}
+			else if (ptr->right != NULL) {
+				ptr = ptr->right;
+				while (ptr->left != NULL)
+					ptr = ptr->left;
+			}
+			else {
+				node_ptr origin = ptr;
+				ptr = ptr->parent;
+				while (ptr && ptr->right == origin) {
+					origin = ptr;
+					ptr = ptr->parent;
+				}
+			}
+			return (*this);
+		}
+		map_const_iterator operator++(int) {
+			map_const_iterator tmp(*this);
+			++(*this);
+			return (tmp);
+		}
+		map_const_iterator& operator--() {
+			if (ptr == NULL) {
+				return (*this);
+			}
+			else if (ptr->left != NULL) {
+				ptr = ptr->left;
+				while (ptr->right != NULL)
+					ptr = ptr->right;
+			}
+			else {
+				node_ptr origin = ptr;
+				ptr = ptr->parent;
+				while (ptr && ptr->left == origin) {
+					origin = ptr;
+					ptr = ptr->parent;
+				}
+			}
+			return (*this);
+		}
+		map_const_iterator operator--(int) {
+			map_const_iterator tmp(*this);
+			--(*this);
+			return (tmp);
+		}
+
+		// ----- PRIVATE -----
+		private:
+
+		// ----- MEMBER TYPES -----
+		typedef ft::map< ft::T::first_type, ft::T::second_type >::Node* node_ptr;
+
+		node_ptr ptr;
+	};
 }
 
 #endif
