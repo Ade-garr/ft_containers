5f7d36a8060e7dcbf6da811fbd5b3ef61588fd0a
Ade-garr <ade-garr@student.42.fr>
2021-11-30 02:59:04 +0100

    map insert

--- includes/map.hpp
+++ includes/map.hpp
@@ -6,14 +6,17 @@
 /*   By: ade-garr <ade-garr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 02:15:57 by ade-garr          #+#    #+#             */
-/*   Updated: 2021/11/23 02:01:51 by ade-garr         ###   ########.fr       */
+/*   Updated: 2021/11/30 02:58:50 by ade-garr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_HPP
 # define MAP_HPP
 
+# include "reverse_iterator.hpp"
+# include "iterator_traits.hpp"
 # include "pair.hpp"
+# include "enable_if.hpp"
 # include <functional>
 
 namespace ft {
@@ -27,8 +30,158 @@ namespace ft {
 			int		height;
 		};
 
+		class value_compare {
+			// friend class map; // a voir
+
+			public:
+
+			typedef bool		result_type;
+			typedef value_type	first_argument_type;
+			typedef value_type	second_argument_type;
+
+			bool operator()(const value_type &x, const value_type &y) const {
+				return comp(x.first, y.first);
+			}
+
+			protected:
+
+			Compare		comp;
+			value_compare(Compare c) : comp(c) {}
+		};
+
+		// ----- PUBLIC -----
 		public:
 
+		// ----- MEMBER TYPES -----
+		typedef Key													key_type;
+		typedef T													mapped_type;
+		typedef ft::pair<const key_type, mapped_type>				value_type;
+		typedef Compare												key_compare;
+		typedef Alloc												allocator_type;
+		typedef typename allocator_type::reference					reference;
+		typedef typename allocator_type::const_reference			const_reference;
+		typedef typename allocator_type::pointer					pointer;
+		typedef typename allocator_type::const_pointer				const_pointer;
+		typedef map_iterator<T>										iterator;
+		typedef map_const_iterator<T>								const_iterator;
+		typedef reverse_iterator<const_iterator>					const_reverse_iterator;
+		typedef reverse_iterator<iterator>							reverse_iterator;
+		typedef typename iterator_traits<iterator>::difference_type	difference_type;
+		typedef size_t												size_type;
+		typedef typename Alloc::template rebind<Node>::other		Node_allocator;
+
+		// ----- MEMBER FUNCTIONS -----
+		explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) : _root(NULL), _lastinsert(NULL), _size(0), _comp(comp), _alloc(alloc) {}
+		template <class InputIterator>
+		map(typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) : _root(NULL), _lastinsert(NULL), _size(0), _comp(comp), _alloc(alloc) {
+			while (first != last)
+				this->insert(*first++);
+		}
+		map(const map& x) : _root(NULL), _lastinsert(NULL), _size(0), _comp(key_compare()), _alloc(allocator_type()) {
+			const_iterator ite = x.end();
+			for (const_iterator it = x.begin(); it != ite; it++)
+				this->insert(*it);
+		}
+		~map() {
+			this->clear();
+		}
+		map& operator=(const map& x) {
+			map tmp(x);
+			this->swap(tmp);
+			return (*this);
+		}
+
+		// ---------- ITERATORS ----------
+		iterator begin() {
+			return (iterator(this->minValueNode(_root)));
+		}
+		const_iterator begin() const {
+			return (const_iterator(this->minValueNode(_root)));
+		}
+		iterator end() {
+			if (this->empty())
+				return (iterator());
+			return (iterator((this->maxValueNode(_root))->right, this->maxValueNode(_root)));
+		}
+		const_iterator end() const {
+			if (this->empty())
+				return (iterator());
+			return (const_iterator((this->maxValueNode(_root))->right, this->maxValueNode(_root)));
+		}
+		reverse_iterator rbegin() {
+			return (reverse_iterator(this->maxValueNode(_root)));
+		}
+		const_reverse_iterator rbegin() const {
+			return (const_reverse_iterator(this->maxValueNode(_root)));
+		}
+		reverse_iterator rend() {
+			if (this->empty())
+				return (reverse_iterator(_root));
+			return (reverse_iterator((this->minValueNode(_root))->left, this->minValueNode(_root)));
+		}
+		const_reverse_iterator rend() const {
+			if (this->empty())
+				return (const_reverse_iterator(_root));
+			return (const_reverse_iterator((this->minValueNode(_root))->left,
+				this->minValueNode(_root)));
+		}
+
+		// ---------- CAPACITY ----------
+		bool empty() const {
+			return (_size == 0);
+		}
+		size_type size() const {
+			return (_size);
+		}
+		size_type max_size() const {
+			return (Node_allocator(_alloc).max_size());
+		}
+
+		// ---------- ELEMENT ACCESS ----------
+		mapped_type& operator[](const key_type& k) {
+			_root = this->tree_insert(_root, NULL, ft::make_pair(k, mapped_type()));
+			// _root = tree_balance(_root); // a voir
+			Node* element = _lastinsert;
+			_lastinsert = NULL;
+			return (element->val.second);
+		}
+
+		// ---------- MODIFIERS ----------
+		pair<iterator,bool> insert(const value_type& val) {
+			size_type size_before = this->size();
+			_root = this->tree_insert(_root, NULL, val);
+			Node* newnode = _lastinsert;
+			_lastinsert = NULL;
+			return (ft::pair<iterator, bool>(iterator(newnode), (this->size() > size_before)));
+		}
+		iterator insert(iterator position, const value_type& val) {
+			if (this->size() < 3)
+				return ((this->insert(val)).first);
+			while (position->first > val.first && position != this->begin())
+				position--;
+			iterator	tmp(position);
+			tmp++;
+			while (position->first < val.first && tmp != this->end())
+			{
+				tmp++;
+				position++;
+			}
+			if (position->first == val.first)
+				return (position);
+			Node* parent = position.getNode()->parent;
+			if (!parent)
+				_root = this->tree_insert(_root, NULL, val);
+			else
+			{
+				tree_insert(position.getNode()->parent, NULL, val);
+				while (_root->parent != NULL)
+					_root = _root->parent;
+				_root = this->tree_balance(_root);
+			}
+			Node*	new_node = _lastinsert;
+			_lastinsert = NULL;
+			return (iterator(new_node));
+		}
 
 		private:
 
